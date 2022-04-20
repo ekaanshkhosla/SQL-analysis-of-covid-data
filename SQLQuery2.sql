920a5cf9f6fa3f051d18c76c00eb99e444db1107
ekaanshkhosla <ekaanshkhosla007@gmail.com>
2022-04-20 23:57:52 +0200

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,3 +1,9 @@
+/*
+Covid 19 Data Exploration 
+Skills used: Joins, CTE's, Aggregate Functions, Creating Views, Converting Data Types
+*/
+
+
 Select *
 From Project..CV --Covid Vaccine
 
@@ -9,20 +15,31 @@ Select Location, date, total_cases, new_cases, total_deaths, population
 From Project..CD
 order by 1,2
 
+
+-- Total Cases vs Total Deaths
+-- Shows likelihood of dying if you contract covid in your country
+
 Select Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DP  --Death Percentage
 From Project..CD
 Where Location like '%Germany%'
 order by 1,2
+
+
 
 Select Location, date, total_cases, population, (total_deaths/population)*100 as DP  --Death Percentage by population
 From Project..CD
 Where Location like '%Germany%'
 order by 1,2
 
+
+-- country with highest infection rate
+
 Select Location, population, MAX(total_cases) as HIC, MAX(total_cases/population)*100 as IP  --HIC: Highest infection count, IP: infection percentage 
 From Project..CD
 Group By location,population
 order by IP desc
+
+
 
 -- Showing countries with Highest Death count per population
 
@@ -31,6 +48,8 @@ From Project..CD
 where continent is not null
 Group By location,population
 order by TDC desc 
+
+
 
 -- Data Analysis by continent
 
@@ -42,6 +61,8 @@ AND location not like '%International%'
 Group By continent
 order by TDC desc 
 
+
+
 -- GLOBAL NUMBERS
 
 Select date, SUM(new_cases) TC, SUM(cast(new_deaths as int)) as TD , SUM(cast(new_deaths as int))/SUM(new_cases)*100 as DP
@@ -49,6 +70,7 @@ From Project..CD
 where continent is not null
 Group By date
 order by 1 
+
 
 
 --  Total Population vs Vaccinations
@@ -62,6 +84,8 @@ Join Project..CD D
 	and V.location = D.location 
 where D.continent is not null
 order by 2,3
+
+
 
 --USE CTE
 
@@ -80,6 +104,8 @@ where D.continent is not null
 )
 Select *, (RPV/Population)*100
 From PopvsVac
+
+
 
 
 --Create view to store data
